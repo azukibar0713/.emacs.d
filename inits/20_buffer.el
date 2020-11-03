cd7bd920d94a2eda7e5c7c15b49045c1376800e5
Shun.Onda <cervelo0713@gmail.com>
2020-11-03 18:28:43 +0900

    iflibpb (switch buffer)

--- inits/20_buffer.el
+++ inits/20_buffer.el
@@ -7,3 +7,9 @@
   (interactive)
   (loop for buffer being the buffers
      do (kill-buffer buffer)))
+
+;;; --------------------------------------------------------------------------------
+;;; バッファを切り替える. mini bufferに切り替えtab風に表示される.
+;;; --------------------------------------------------------------------------------
+(global-set-key (kbd "M-o") 'iflipb-next-buffer)
+(global-set-key (kbd "M-O") 'iflipb-previous-buffer)
