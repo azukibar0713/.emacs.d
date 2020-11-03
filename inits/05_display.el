82dade85fe314ba68fdfd958efe55dacf445a063
Shun.Onda <cervelo0713@gmail.com>
2020-11-03 17:05:09 +0900

    show space, tab at line end

--- inits/05_display.el
+++ inits/05_display.el
@@ -25,3 +25,22 @@
 ;; フェイスを変更する
 ;;(set-face-background 'show-paren-match-face nil)
 ;;(set-face-underline-p 'show-paren-match-face "yellow")
+
+
+
+;;; --------------------------------------------------------------------------------
+;;; 行末のタブ、スペースを可視化.
+;;; --------------------------------------------------------------------------------
+(require 'whitespace)
+(setq whitespace-style '(face           ; faceで可視化
+                         trailing       ; 行末
+                         tabs           ; タブ
+;;                         empty          ; 先頭/末尾の空行
+                         space-mark     ; 表示のマッピング
+                         tab-mark
+                         ))
+
+(setq whitespace-display-mappings
+      '((tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+
+(global-whitespace-mode 1)
