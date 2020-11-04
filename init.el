d0dbf8dda9cbd21988c0f6cc9b79a01c307f1e84
Shun.Onda <cervelo0713@gmail.com>
2020-11-05 01:35:51 +0900

    open junk

--- init.el
+++ init.el
@@ -5,8 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(company company-c-headers helm-migemo migemo ace-isearch ace-jump-buffer ace-jump-mode helm-swoop helm-git-files helm-git-grep helm-ls-git package-utils init-loader)))
+ '(package-selected-packages '(open-junk-file package-utils init-loader)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
