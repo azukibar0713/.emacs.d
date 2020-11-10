2db429ddacad846d49b924ab12fd900cd22f0bdf
Shun.Onda <cervelo0713@gmail.com>
2020-11-11 01:05:24 +0900

    yasnippets

--- inits/00_util.el
+++ inits/00_util.el
@@ -35,21 +35,7 @@
 ;;; recentf拡張
 ;;; http://d.hatena.ne.jp/rubikitch/20091224/recentf
 ;;; --------------------------------------------------------------------------------
-(defmacro with-suppressed-message (&rest body)
-  "Suppress new messages temporarily in the echo area and the `*Messages*' buffer while BODY is evaluated."
-  (declare (indent 0))
-  (let ((message-log-max nil))
-    `(with-temp-message (or (current-message) "") ,@body)))
-
-(require 'recentf)
-(setq recentf-save-file "~/.emacs.d/.recentf")
-(setq recentf-max-saved-items 1000)            ;; recentf に保存するファイルの数
-(setq recentf-exclude '(".recentf"))           ;; .recentf自体は含まない
-(setq recentf-auto-cleanup 'never)             ;; 保存する内容を整理
-(run-with-idle-timer 60 t '(lambda ()          ;; 60秒ごとに .recentf を保存
-   (with-suppressed-message (recentf-save-list))))
 (require 'recentf-ext)
-
 
 ;;; --------------------------------------------------------------------------------
 ;;; ローマ字で日本語検索.
