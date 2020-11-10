04fb224a1889b6ae264b6bcfa43411b68c113bd5
Shun.Onda <cervelo0713@gmail.com>
2020-11-11 01:41:29 +0900

    load manual el at first

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; ~/.emacs.d/site-lisp 以下全部読み込み
+(let ((default-directory (expand-file-name "~/.emacs.d/manual_install")))
+  (add-to-list 'load-path default-directory)
+  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+      (normal-top-level-add-subdirs-to-load-path)))
+
 (package-initialize)
 (init-loader-load)
 (custom-set-variables
--- inits/60_yasnippet.el
+++ inits/60_yasnippet.el
@@ -1,15 +1,15 @@
 ;; 自分用・追加用テンプレート -> mysnippetに作成したテンプレートが格納される
 (setq yas-snippet-dirs
-      '("../yasnippets_my"
-        "../yasnippets"
+      '("~/.emacs.d/yasnippets_my"
+        "~/.emacs.d/yasnippets"
         ))
 
 ;; 既存スニペットを挿入する
-(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+;(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
 ;; 新規スニペットを作成するバッファを用意する
-(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+;(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
 ;; 既存スニペットを閲覧・編集する
-(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+;(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
 
 (yas-global-mode 1)
 
