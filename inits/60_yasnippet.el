b83b0e4a7da4d9c829e9f34fd53c453ca275a3aa
Shun.Onda <cervelo0713@gmail.com>
2020-11-11 01:54:20 +0900

    fix yasnippet.el

--- inits/60_yasnippet.el
+++ inits/60_yasnippet.el
@@ -1,3 +1,4 @@
+(require 'yasnippet)
 ;; 自分用・追加用テンプレート -> mysnippetに作成したテンプレートが格納される
 (setq yas-snippet-dirs
       '("~/.emacs.d/yasnippets_my"
@@ -5,11 +6,11 @@
         ))
 
 ;; 既存スニペットを挿入する
-;(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
 ;; 新規スニペットを作成するバッファを用意する
-;(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
 ;; 既存スニペットを閲覧・編集する
-;(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
 
 (yas-global-mode 1)
 
