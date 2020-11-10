9bf2aaf6805d62371b6b522fa05cd24e829cfc77
Shun.Onda <cervelo0713@gmail.com>
2020-11-11 02:01:26 +0900

    undo hist

--- init.el
+++ init.el
@@ -11,7 +11,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(yasnippet open-junk-file package-utils init-loader)))
+ '(package-selected-packages
+   '(undohist yasnippet open-junk-file package-utils init-loader)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
