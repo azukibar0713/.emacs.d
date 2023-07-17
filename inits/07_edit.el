629b08f01b18d99ddaaee4ae5ee4b0622d38abcc
azukibar0713 <cervelo0713@gmail.com>
2023-07-17 17:58:06 +0900

    multi cursor key bind

--- inits/07_edit.el
+++ inits/07_edit.el
@@ -60,6 +60,6 @@
 
 
 (require 'multiple-cursors)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "M->") 'mc/mark-next-like-this)
+(global-set-key (kbd "M-<") 'mc/mark-previous-like-this)
 
